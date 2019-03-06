7ccfc7822b1a4535a63bd1641dca26bd3ec1a5a9
Unknown <cbrown12@umassd.edu>
2019-03-05 23:21:15 -0500

    Update a few metrics

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ Colin Brown
 CIS602
  */
 
-
 d3.json('https://raw.githubusercontent.com/colinjbrown/cis602hw/master/data/nodes.json').then(function (data) {
 
     var width = 600;
@@ -16,12 +15,15 @@ d3.json('https://raw.githubusercontent.com/colinjbrown/cis602hw/master/data/node
         $('#affinity2').text('');
         $('#node1').text('');
         $('#node2').text('');
-        d3.selectAll('circle').attr('cx', function (d) {
+        d3.selectAll('circle')
+            .attr('cx', function (d) {
             return Math.floor((Math.random() * 200) + width / 2)
-        })
+            })
             .attr('cy', function (d) {
                 return Math.floor((Math.random() * 200) + height / 2)
-            })
+            });
+        //Since this design relies on distances it's important to reset all nodes on a full reset
+        svg2.selectAll('circle').classed('not-active', true).classed('active2', false).classed('lowaff', false).classed('highaff', false).classed('midaff', false);
     }
 
     //Interaction 1 Starts here
@@ -71,7 +73,7 @@ d3.json('https://raw.githubusercontent.com/colinjbrown/cis602hw/master/data/node
                 return move;
             }).attr('cy', function (d) {
             var affinity = relation[parseInt(d[1])];
-            if (affinity < .5) {
+            if (affinity > .5) {
                 affinity = -affinity;
             } else if (affinity == .5) {
                 affinity = 0;
@@ -143,30 +145,46 @@ d3.json('https://raw.githubusercontent.com/colinjbrown/cis602hw/master/data/node
         .attr('r', 5)
         .on('click', function (d) {
 
+            svg2.selectAll('circle').classed('lowaff', false).classed('highaff', false).classed('midaff', false);
             d3.select('.active2').classed('active2', false).classed('not-active', true);
 
             if (!clicked || d != active2) {
+
+
                 $('#node2').text(d);
                 d3.select(this).classed('active2', true).classed('not-active', false).transition().duration(1000).attr('cx', width / 2).attr('cy', height / 2);
                 relation2 = data[d];
 
+                svg2.selectAll('circle.not-active').attr('class', function (d) {
+                    var affinity = relation2[parseInt(d[1])];
+                    if (affinity < .5) {
+                        return 'not-active lowaff';
+                    } else if (affinity == .5) {
+                        return 'not-active midaff';
+                    }
+                    return 'not-active highaff';
+                });
+
+
                 x = parseInt(d3.select(this).attr('cx'));
                 y = parseInt(d3.select(this).attr('cy'));
 
+                //Generate random angles from 0 to 2 pi
                 angles = Array.from({length: 10}, () => Math.random() * 2 * Math.PI);
 
                 svg2.selectAll('circle.not-active').transition().duration(1000).attr('cx', function (d) {
                     var idx = parseInt(d[1]);
-                    return x + scaling_factor * (Math.cos(angles[idx]) * (width / 2) * (1 - relation2[idx]));
+                    return x + (scaling_factor * (Math.cos(angles[idx]) * (width / 2) * (1 - relation2[idx])));
                 }).attr('cy', function (d) {
                     var idx = parseInt(d[1]);
-                    return y + scaling_factor * (Math.sin(angles[idx]) * (height / 2) * (1 - relation2[idx]));
+                    return y + (scaling_factor * (Math.sin(angles[idx]) * (height / 2) * (1 - relation2[idx])));
                 });
 
                 clicked = true;
                 active2 = d;
             } else {
                 clicked = false;
+                active2 = null;
                 reset();
             }
 
@@ -179,7 +197,5 @@ d3.json('https://raw.githubusercontent.com/colinjbrown/cis602hw/master/data/node
             d3.select(this).style('fill', null);
         });
     ;
-
-
 });
 
